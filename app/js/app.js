f76d16c393eb2d95d268b00f46ff3da103f4b607
Morhys <morhys.m@googlemail.com>
2014-08-21 00:21:41 +0100

    css tidy up

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 // Set as a variable to use later on.
 var eventsApp = angular.module('eventsApp', ['ngSanitize']);
